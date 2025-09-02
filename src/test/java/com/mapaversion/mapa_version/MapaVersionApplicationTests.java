aa72a6b9fb0c301492d1fbeeae852209cd0ac90e
José <josepaulo1607@gmail.com>
2025-09-01 21:07:18 -0300

    initial commit

---
+++ src/test/java/com/mapaversion/mapa_version/MapaVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapaversion.mapa_version;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapaVersionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
